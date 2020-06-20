cae8bb1b67db52363dbe7fd1b2cc85a4dcabbdd8
adnankhalid46 <adnankhalid46@gmail.com>
2020-06-20 12:03:56 +0500

    updated

---
+++ src/components/Transaction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export const Transaction=()=>{
+        return(
+            <li className="plus">
+                        Project 1 Income
+                        <span>$1000</span>
+                        <button className="delete-btn">X</button>
+                    </li>
+
+        )
+
+}
--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {GlobalContext} from '../context/GlobalState';
+import {Transaction} from "./Transaction"
 
 export const TransactionHistory=()=>{
+
+    const {transactions }= useContext(GlobalContext);
+    console.log(transactions)
+
         return(
             <div>
                 <h3>Transaction Histroy</h3>
                 <ul className="list">
-                    <li className="plus">
-                        Project 1 Income
-                        <span>$1000</span>
-                        <button classNae="delete-btn">X</button>
-                    </li>
-                    <li className="minus"></li>
-                        Project 1 Salaries
-                        <span>$500</span>
-                        <button classNae="delete-btn">X</button>
+                    <Transaction />
+
                 </ul>
             </div>
         )
