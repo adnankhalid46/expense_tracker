1c12f65f4f9d5ebcb76aab9902e7937aa7620b59
adnankhalid46 <adnankhalid46@gmail.com>
2020-06-20 11:27:51 +0500

    updated

---
+++ src/context/GlobalState.js
@@ -0,0 +1,32 @@
+import React, {createContext,useReducer} from 'react'
+//Import the Reducer
+import AppReducer from './AppReducer'
+
+//Create the initial state
+const initialState={
+    transaction: [
+        {id: 1, desscrition: "Income 1", transactionAmount: 1000},
+        {id: 1, desscrition: "Expense 1", transactionAmount: -100},
+        {id: 1, desscrition: "Income 2", transactionAmount: 2000},
+        {id: 1, desscrition: "Expense 2", transactionAmount: -500}
+    ]
+}
+
+//Create the Global Context
+export const GlobalContext= createContext(initialState);
+
+//Create a Provider for the Global Context
+export const GlobalProvider =({children}) =>{
+    
+    const [state,dispatch]=useReducer(AppReducer, initialState)
+
+    return (
+        <GlobalContext.Provider value={
+            {
+            transactions: StaticRange.transactions
+            }
+        }>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
